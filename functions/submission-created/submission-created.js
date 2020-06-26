fb4c2a5f4e16a4b3224e89eaa041d0ad507294f4
Perttu Lähteenlahti <perttu.lahteenlahti@gmail.com>
2020-06-26 13:04:56 +0300

    new function

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,7 +1,58 @@
 import { createPass, Pass } from "passkit-generator"
-
+import * as path from "path";
 exports.handler = function (event, context, callback) {
   const response = JSON.parse(event)
   console.log(response)
+
+
+  try {
+    //NOTE: using __dirname is required inside lambda
+
+    const examplePass = await createPass({
+      model: path.resolve(__dirname, "/models/exampleBooking"),
+      certificates: {
+        wwdr: path.resolve(__dirname, "/certs/wwdr.pem"),
+        signerCert: path.resolve(__dirname, "/certs/signercert.pem"),
+        signerKey: {
+          keyFile: path.resolve(__dirname, "/certs/signerkey.pem"),
+          passphrase: "123456"
+        }
+      },
+      overrides: {
+        // keys to be added or overridden
+        serialNumber: "AAGH44625236dddaffbda"
+      }
+    });
+
+
+    // Generate the stream, which gets returned through a Promise
+    const stream = examplePass.generate();
+
+    // Important: this is needed for API GW to work
+    const pass = stream.toString();
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Content-type": "application/vnd.apple.pkpass",
+        "Content-disposition": `attachment; filename=${passName}.pkpass`
+      },
+      body: pass,
+      isBase64Encoded: true
+    };
+
+    // To debug in Cloudwatch
+    console.log(response);
+    callback(null, response);
+
+    return response;
+  } catch (error) {
+    return {
+      statusCode: 500,
+      error: `Could not get: ${error.stack}`
+    };
+  }
   // your server-side functionality
 }
